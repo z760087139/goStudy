e07b3e2ecbb03bc236cdc5461d5caacb5a6f028a
bigdog <z760087139@163.com>
2020-04-24 09:38:09 +0800

    更新第六节课

--- class6/code1/main.go
+++ class6/code1/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
 
 // 标准函数形式
 
@@ -46,8 +51,71 @@ func f4(k bool) (name string, age int) {
 	return
 }
 
+// 递归函数 可以在函数内部调用自身，形成递归
+func f5(x int) {
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f5(x - 1)
+}
+
+// 错误接收及一般处理策略
+// GO语言里面没有像其他语言一样的异常处理机制，比如python 里面的try except。
+// 所有的已知错误都需要通过error进行传递，发生未知错误时使用panic
+func f6() {
+	// 比如在打开文件时候，会返回error
+	f, err := os.Open("error.txt")
+	if err != nil {
+		// 错误类型可以直接打印
+		fmt.Println(err)
+		// fmt.Printf("%v", err)
+	}
+	defer f.Close()
+}
+
+// 错误处理方式一  上报且添加信息
+// 传入文件名，打开并读取文件内容
+func f7(filename string) ([]byte, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		// 错误时，返回nil ，并用fmt.Errorf 拼接新的错误内容
+		return nil, fmt.Errorf("Open %s Failed : %v", filename, err)
+	}
+	defer f.Close()
+	b, _ := ioutil.ReadAll(f)
+	return b, nil
+}
+
+// 错误处理方式二  重试
+func f8(filename string) ([]byte, error) {
+	// 重复次数
+	const t = 3
+	for tries := 0; tries < t; tries++ {
+		b, err := f7(filename)
+		if err == nil {
+			return b, nil
+		}
+		fmt.Println(err, "retry...")
+	}
+	// 根据时间重试
+	const timeout = 3 * time.Second
+	deadline := time.Now().Add(timeout)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		b, err := f7(filename)
+		if err == nil {
+			return b, nil
+		}
+		fmt.Println(err, "retry by time")
+		time.Sleep(time.Second)
+	}
+	return nil, fmt.Errorf("Open Failed in %d times", t)
+}
+
 func main() {
 	// f1(1, 2, 3, 4, 5, 6)
-	fmt.Println(f4(true))
-	fmt.Println(f4(false))
+	// fmt.Println(f4(true))
+	// fmt.Println(f4(false))
+	_, err := f8("error.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
